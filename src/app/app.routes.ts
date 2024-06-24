bf314b4cda6bc093cbae3a3287708cbd3e54eddb
Jon Quass <quassj@gmail.com>
2024-06-24 12:10:45 -0400

    Add Three Basic Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {AccountComponent} from "./account/account.component";
+import {UploadComponent} from "./upload/upload.component";
+import {TransactionComponent} from "./transaction/transaction.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'accounts', component: AccountComponent},
+  {path: 'upload', component: UploadComponent},
+  {path: 'transactions', component: TransactionComponent}
+];
---
+++ src/app/transaction/transaction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaction.component.html',
+  styleUrl: './transaction.component.css'
+})
+export class TransactionComponent {
+
+}
