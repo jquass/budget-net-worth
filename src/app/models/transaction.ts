ca7b496a63e0db38554ca1a927e67bf2cfc9b4c9
Jon Quass <quassj@gmail.com>
2024-06-25 16:27:27 -0400

    Fix transactions page

---
+++ src/app/models/transaction.ts
@@ -0,0 +1,9 @@
+export interface Transaction {
+  id: number;
+  date: string[];
+  memo: string;
+  amount: number;
+  accountId: number;
+  uploadId: number;
+  uploadRowId: number;
+}
--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {Transaction} from "../models/transaction";
+import {NgForOf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {Account} from "../models/account";
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css'
 })
 export class TransactionComponent {
+  public transactions: Transaction[] = [];
 
+  public amountFormatter : Intl.NumberFormat = new Intl.NumberFormat('en-US',
+    {style: 'currency', currency: 'USD'});
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Transaction[]>("http://localhost:8080/api/transactions")
+      .subscribe(transactions => {
+        this.transactions = transactions;
+      });
+  }
 }
